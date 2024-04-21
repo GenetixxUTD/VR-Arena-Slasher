c7e38565725ab21d8eb6a0d54432a7f7d02a896d
GenetixxUTD <cameronshirley8@googlemail.com>
2024-04-21 21:46:33 +0100

    Player Movement Created

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 1.0f;
+    public XRNode inputController;
+    public XROrigin playerRig;
+    private Vector2 InputAxis;
+    private CharacterController character;
+
+    public float gravity = -9.81f;
+    private float downwardSpeed;
+    public LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        playerRig = GetComponent<XROrigin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputController);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out InputAxis);
+    }
+
+    private void FixedUpdate()
+    {
+        Quaternion headDir = Quaternion.Euler(0, playerRig.Camera.transform.eulerAngles.y, 0);
+        Vector3 movementDirection = headDir * new Vector3(InputAxis.x, 0, InputAxis.y);
+        character.Move(movementDirection * Time.fixedDeltaTime * movementSpeed);
+
+        if(isGrounded())
+        {
+            downwardSpeed = 0;
+        }
+        else
+        {
+            downwardSpeed += gravity * Time.fixedDeltaTime;
+        }
+        
+        character.Move(Vector3.up * downwardSpeed * Time.fixedDeltaTime);
+    }
+
+    private bool isGrounded()
+    {
+        Vector3 sphereStart = transform.TransformPoint(character.center);
+        float sphereRayLength = character.center.y + 0.01f;
+        bool hasHit = Physics.SphereCast(sphereStart, character.radius, Vector3.down, out RaycastHit hit, sphereRayLength, groundLayer);
+        return hasHit;
+    }
+}
