503f0ea0ad07c7c1d4494f5102347dfd28c4bd79
GenetixxUTD <cameronshirley8@googlemail.com>
2024-04-22 02:39:56 +0100

    Main Menu Scene Implemented

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public float rotationRate;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotationRate, 0);
+    }
+
+    public void toGameScene()
+    {
+        SceneManager.LoadScene("Game Scene");
+    }
+
+    public void toQuitGame()
+    {
+        Application.Quit();
+    }
+}
