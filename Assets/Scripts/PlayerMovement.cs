6952645487686537a608590cc14b52d08a6753c6
GenetixxUTD <cameronshirley8@googlemail.com>
2024-04-26 04:29:59 +0100

    Added teleportation movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.XR;
@@ -17,9 +18,20 @@ public class PlayerMovement : MonoBehaviour
     private float downwardSpeed;
     public LayerMask groundLayer;
 
+    public GameObject TeleportationHandManager;
+
+    public enum movementType
+    {
+        continuous,
+        teleport
+    }
+
+    public movementType selectedMovement;
+
     // Start is called before the first frame update
     void Start()
     {
+        
         character = GetComponent<CharacterController>();
         playerRig = GetComponent<XROrigin>();
     }
@@ -27,6 +39,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(selectedMovement == movementType.continuous)
+        {
+            Quaternion headDir = Quaternion.Euler(0, playerRig.Camera.transform.eulerAngles.y, 0);
+            Vector3 movementDirection = headDir * new Vector3(InputAxis.x, 0, InputAxis.y);
+            character.Move(movementDirection * Time.fixedDeltaTime * movementSpeed);
+            continuousMovement();
+            TeleportationHandManager.SetActive(false);
+        }
+        else
+        {
+            TeleportationHandManager.SetActive(true);
+        }
         InputDevice device = InputDevices.GetDeviceAtXRNode(inputController);
         device.TryGetFeatureValue(CommonUsages.primary2DAxis, out InputAxis);
     }
@@ -34,9 +58,8 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         colliderHeadsetPos();
-        Quaternion headDir = Quaternion.Euler(0, playerRig.Camera.transform.eulerAngles.y, 0);
-        Vector3 movementDirection = headDir * new Vector3(InputAxis.x, 0, InputAxis.y);
-        character.Move(movementDirection * Time.fixedDeltaTime * movementSpeed);
+        continuousMovement();
+        
 
         if(isGrounded())
         {
@@ -63,5 +86,15 @@ public class PlayerMovement : MonoBehaviour
         character.height = playerRig.CameraInOriginSpaceHeight;
         Vector3 centerHeight = transform.InverseTransformPoint(playerRig.Camera.transform.position);
         character.center = new Vector3(centerHeight.x, character.height / 2, centerHeight.z);
+    }
+
+    public void fetchSettings()
+    {
+
+    }
+
+    private void continuousMovement()
+    {
+        
     }
 }
